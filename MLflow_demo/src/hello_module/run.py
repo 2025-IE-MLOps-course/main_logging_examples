820460b93b9fa7ea56cbdbcd281205308c812877
Ivan Diaz <phi.designer@gmail.com>
2025-05-27 23:47:44 +0200

    Update default message in run.py to remove exclamation mark

--- MLflow_demo/src/hello_module/run.py
+++ MLflow_demo/src/hello_module/run.py
@@ -8,6 +8,6 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--message", type=str,
-                        default="Hello from hello_module!")
+                        default="Hello from hello_module")
     args = parser.parse_args()
     main(args)
